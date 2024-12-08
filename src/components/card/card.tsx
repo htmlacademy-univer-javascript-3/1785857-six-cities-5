274644cb9298c0e53794e4050ff11b0cf91f0979
ruslan0003 <ruslan0003@yandex.ru>
2024-12-08 14:23:47 +0300

    fix: Добавить деструктуризацию в Card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { CardType } from '../../types/card';
 import { Link } from 'react-router-dom';
 
-function Card({imageUrl, header, price, type, isPremium, id}: CardType): JSX.Element {
+function Card({...offers}: CardType): JSX.Element {
 
   const [isCardActive, setCardActive] = useState(false);
   // Вывод в консоль – это временное решение, чтобы линтер не ругался на неиспользуемую переменную isCardActive
@@ -11,16 +11,16 @@ function Card({imageUrl, header, price, type, isPremium, id}: CardType): JSX.Ele
 
   return(
     <article className="cities__card place-card" onMouseEnter={() => setCardActive(true)} onMouseLeave={() => setCardActive(false)} >
-      {isPremium ? <div className="place-card__mark"><span>Premium</span></div> : null}
+      {offers.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : null}
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <Link to={`/offer/${id}`}>
-          <img className="place-card__image" src={imageUrl} width="260" height="200" alt={header} />
+        <Link to={`/offer/${offers.id}`}>
+          <img className="place-card__image" src={offers.imageUrl} width="260" height="200" alt={offers.header} />
         </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
+            <b className="place-card__price-value">&euro;{offers.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -37,9 +37,9 @@ function Card({imageUrl, header, price, type, isPremium, id}: CardType): JSX.Ele
           </div>
         </div>
         <h2 className="place-card__name">
-          <Link to={`/offer/${id}`}>{header}</Link>
+          <Link to={`/offer/${offers.id}`}>{offers.header}</Link>
         </h2>
-        <p className="place-card__type">{type}</p>
+        <p className="place-card__type">{offers.type}</p>
       </div>
     </article>
   );
